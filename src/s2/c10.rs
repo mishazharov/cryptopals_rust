fd0af913458c5e26417157c6b29eda737de2b9fe
Misha Zharov <mishazharov1@gmail.com>
2020-01-01 15:06:29 -0500

    Added PKCS #7 to c10

--- src/s2/c10.rs
+++ src/s2/c10.rs
@@ -8,6 +8,7 @@ use crate::s1::c5::xor_encrypt;
 
 const AES_BLOCK_SIZE: usize = 16;
 
+// Decrypts AES CBC data assuming that it is using PKCS #7 padding
 fn aes_cbc_decrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8> {
     let mut decrypter = Crypter::new(
         Cipher::aes_128_ecb(),
@@ -28,6 +29,12 @@ fn aes_cbc_decrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8> {
     plaintext.truncate(count);
     let slice_end = data.len() - 16;
     xor_encrypt(&data[..slice_end], &mut plaintext[16..]);
+
+    // Remove PKCS #7 padding
+    let padding_length = plaintext.last().unwrap();
+    let final_length = plaintext.len().saturating_sub(*padding_length as usize);
+    plaintext.truncate(final_length);
+
     plaintext
 }
 
@@ -43,12 +50,11 @@ mod tests {
         let key = b"YELLOW SUBMARINE";
         let iv = [0u8; AES_BLOCK_SIZE];
         
-        // xor_encrypt(&raw_bytes, &mut res);
         let plaintext = aes_cbc_decrypt(&raw_bytes, key, &iv);
 
         assert_eq!(
             hex::decode(
-                "368f2b80b437209451355b750181b378f425cc00af3922bcecc8d4a7d84a5198"
+                "24df84533fc2778495577c844bcf3fe1d4d17c68d8c5cbc5a308286db58c69b6"
             ).unwrap(),
             sha::sha256(&plaintext)
         );
