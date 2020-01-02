ef7f854382e84cee73d21f3b3746a35c8555886f
Misha Zharov <mishazharov1@gmail.com>
2020-01-01 23:57:06 -0500

    Got a Windows built to work

---
+++ build.bat
@@ -0,0 +1,8 @@
+:: This is a build script for windows. On other platforms you can
+:: just run `cargo test`
+::
+:: Prior to using this script:
+:: * install x86-64 static OpenSSL
+:: * make sure that you are using the x86-64 rust toolchain
+set RUSTFLAGS=-Ctarget-feature=+crt-static
+cargo test
