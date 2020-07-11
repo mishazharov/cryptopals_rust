823219e48161aea2ff77494c210c1335ef52b4db
Misha Zharov <mishazharov1@gmail.com>
2020-07-10 23:49:25 -0400

    s4c28: Add tests

---
+++ src/s4/c28.rs
@@ -0,0 +1,69 @@
+use crate::sha1::*;
+
+fn sha1_keyed_mac(content: &[u8], key: &[u8]) -> Vec<u8> {
+    let mut to_hash = key.to_vec();
+    to_hash.extend_from_slice(&content);
+    sha1(&to_hash)
+}
+
+fn sha1_verify_mac(content: &[u8], key: &[u8], hash: &[u8]) -> bool {
+    &sha1_keyed_mac(content, key)[..] == hash
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mac_success() {
+        let content = "Intruder alert! Red Spy is in the base!".as_bytes();
+        let key = "mentlegen".as_bytes();
+
+        let hash = sha1_keyed_mac(
+            content,
+            key
+        );
+
+        assert!(sha1_verify_mac(content, key, &hash));
+    }
+
+    #[test]
+    fn test_mac_fail() {
+        let content = "Intruder alert! Red Spy is in the base!".as_bytes();
+        let key = "mentlegen".as_bytes();
+
+        let hash = sha1_keyed_mac(
+            content,
+            key
+        );
+
+        assert!(
+            !sha1_verify_mac(
+                // Missing exclamation mark
+                "Intruder alert! Red Spy is in the base".as_bytes(),
+                key,
+                &hash
+            )
+        );
+    }
+
+    #[test]
+    fn test_need_key_for_mac() {
+        let content = "Intruder alert! Red Spy is in the base!".as_bytes();
+        let key = "mentlegn".as_bytes();
+
+        let hash = sha1_keyed_mac(
+            content,
+            key
+        );
+
+        assert!(
+            !sha1_verify_mac(
+                // Missing exclamation mark
+                content,
+                "mentlegen".as_bytes(),
+                &hash
+            )
+        );
+    }
+}
--- src/s4/mod.rs
+++ src/s4/mod.rs
@@ -1,3 +1,4 @@
 mod c25;
 mod c26;
 mod c27;
+mod c28;
